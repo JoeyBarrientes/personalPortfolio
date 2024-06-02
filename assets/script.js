99f665ad423ac5e1bec55b315df735c3b928037b
JoeyBarrientes <142752045+JoeyBarrientes@users.noreply.github.com>
2024-06-01 22:46:45 -0500

    Implement Version 1.1
    -Fixed off-screen scrolling on mobile
    -Fully implemented color change based on scroll location for every obvious element (nav, menu, home, nearby text)
    -Implemented subtle element movement on page load for hero and section text
    -Further optimized mobile appearance
    -Further optimized and cleaned up code and lines, especially JavaScript
    
    TODO:
    -Further optimization and clean up on mobile appearance and files
    -Fill in for placeholder projects
    -Possibly experiment with other layouts/colors/appearance elements

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,250 @@
-
-/* ---------- Slideshow ---------- */
+/* ----------  Global --------------------- */
+var isLight = true;
+var hasChanged = 0;
 let currentSlide = 0;
-let isLight = true;
-let hasChanged = 0;
+const navHoverInterval = 100;
+const slideInterval = 4000;
 
+
+/* *********** USER CONTROLLED EVENTS  *********** */
+/* *********************************************** */
+
+/* ----------  Window Scroll --------------------- */
+window.addEventListener('scroll', function() {
+    colorModeControl(); 
+    updateParallax();
+});
+
+/* ----------  Window Resize --------------------- */
+window.addEventListener('resize', function() {
+    switchNav();
+    updateParallax();
+});
+
+/* ---------- Burger Menu Click --------------------- */
+document.getElementById('burgerMenu').addEventListener('click', function() {
+    menuClick();
+});
+
+
+/* *************** DOM CONTENT LOAD  **************** */
+/* ************************************************** */
+document.addEventListener('DOMContentLoaded', function() {
+    colorModeControl();
+    transformOnLoad();
+    setInterval(hoverNav(),navHoverInterval);
+    showSlide(currentSlide);
+    setInterval(() => {
+        moveSlide(1);
+    }, slideInterval);
+    window.addEventListener('resize', () => {
+        showSlide(currentSlide);
+    });
+    updateParallax();
+});
+
+
+/* *************** FUNCTIONS  **************** */
+/* -------------------------------------------------- */
+
+/* --------------------------------------------------------------------------- */
+function colorModeControl() {
+    const scrollPosition = window.scrollY;
+    const sectionHeaders = document.getElementsByTagName('h1');
+    const sectionNums = document.querySelectorAll('h1 span');
+    const aboutText = document.querySelectorAll('#about p');
+    const contactText = document.querySelectorAll('#contact p, #contact a');
+    const burgerMenu = document.getElementById('burgerMenu');
+    const homeButton = document.querySelector('.homeButton img');
+
+    function lightScreen() {
+        isLight = true;
+        document.body.style.backgroundColor = 'var(--bgColor)';
+        for (let i = 0; i < sectionHeaders.length; i++) {
+            sectionHeaders[i].style.color = 'black';
+        }
+        sectionNums.forEach(num => {
+            num.style.color = 'var(--tertiaryColor)';
+        });
+        aboutText.forEach(text => {
+            text.style.color = 'black';
+        });
+        contactText.forEach(contactText => {
+            contactText.style.color = 'black';
+        });
+
+        burgerMenu.style.color = 'black';
+        homeButton.src = 'assets/images/homeDark.svg';
+    }
+
+    function darkScreen() {
+        isLight = false;
+        document.body.style.backgroundColor = 'var(--secondaryBgColor)';
+        for (let i = 0; i < sectionHeaders.length; i++) {
+            sectionHeaders[i].style.color = 'var(--bgColor)';
+        }
+        sectionNums.forEach(num => {
+            num.style.color = 'var(--primaryShadow)';
+        });
+        aboutText.forEach(aboutText => {
+            aboutText.style.color = 'var(--bgColor)';
+        });
+        contactText.forEach(contactText => {
+            contactText.style.color = 'var(--bgColor';
+        });
+        burgerMenu.style.color = 'var(--bgColor)';
+        homeButton.src = 'assets/images/homeLight.svg';        
+    }
+
+    function updateNavColor() {
+        const navLinks = document.querySelectorAll('.navLink a');
+        
+        navLinks.forEach(link => {
+    
+            if ((isLight) && (hasChanged < 4)) {
+                link.classList.remove('navDark');
+                link.classList.add('navLight');
+                hasChanged++;
+            } else if ((!isLight) && (hasChanged > 0)){
+                link.classList.remove('navLight');
+                link.classList.add('navDark');
+                hasChanged--;}
+        });
+
+        if ((isLight)) {
+            burgerMenu.classList.add('burgerLight');
+            burgerMenu.classList.remove('burgerDark');
+
+            homeButton.classList.add('homeLight');
+            homeButton.classList.remove('homeDark');
+        } else if ((!isLight)) {
+            burgerMenu.classList.add('burgerDark');
+            burgerMenu.classList.remove('burgerLight');
+
+            homeButton.classList.add('homeDark');
+            homeButton.classList.remove('homeLight');
+        }
+    }
+
+    function initiateColorChange() {
+        if (window.innerWidth > 500){
+    
+            if (scrollPosition <= 547) {
+                lightScreen();
+                updateNavColor();
+            } else if (547 < scrollPosition && scrollPosition <= 1426) {
+                lightScreen();
+                updateNavColor();
+            } else if (1426 < scrollPosition && scrollPosition <= 3413) {
+                darkScreen();
+                updateNavColor();
+            } else {
+                lightScreen();
+                updateNavColor();
+            }
+    
+        } else{
+            
+            if (scrollPosition <= 309) {
+                lightScreen();
+                updateNavColor();
+            } else if (309 < scrollPosition && scrollPosition <= 1388) {
+                lightScreen();
+                updateNavColor();
+            } else if (1388 < scrollPosition && scrollPosition <= 3460) {
+                darkScreen();
+                updateNavColor();
+            } else {
+                lightScreen();
+                updateNavColor();
+            }
+        }
+    }
+
+    initiateColorChange();
+}
+
+/* --------------------------------------------------------------------------- */
+function menuClick() {
+    var navigation = document.querySelector('.navigation');
+    var navLink = document.querySelector('.navContainer > ul');
+    if (navigation.style.display === 'flex') {
+        navigation.style.display = 'none';
+        navLink.style.display = 'none';
+    } else {
+        navigation.style.display = 'flex';
+        navLink.style.display = 'flex';
+    }
+}
+
+/* --------------------------------------------------------------------------- */
+function hoverNav() {
+    const burgerMenu = document.getElementById('burgerMenu');
+    const homeButton = document.querySelector('.homeButton img');
+    const navLinks = document.querySelectorAll('.navLink a');
+
+    navLinks.forEach(link => {
+        if ((isLight))  {
+            link.classList.add('navLight');
+            link.classList.remove('navDark');
+        } else if ((!isLight)) {
+            link.classList.add('navDark');
+            link.classList.remove('navLight');
+        }
+    });
+
+    if ((isLight)) {
+        burgerMenu.classList.add('burgerLight');
+        burgerMenu.classList.remove('burgerDark');
+
+        homeButton.classList.add('homeLight');
+        homeButton.classList.remove('homeDark');
+    } else if ((!isLight)) {
+        burgerMenu.classList.add('burgerDark');
+        burgerMenu.classList.remove('burgerLight');
+
+        homeButton.classList.add('homeDark');
+        homeButton.classList.remove('homeLight');
+    }
+}
+
+/* --------------------------------------------------------------------------- */
+function transformOnLoad() {
+    // Select the text element that will pop up
+    const firstName = document.querySelector('.firstName');
+    const lastName = document.querySelector('.lastName');
+    const firstHero = document.querySelector('.firstHero');
+    const secondHero = document.querySelector('.secondHero');
+    const pageElements = document.querySelectorAll('#about, #skills, #projects, #contact');
+
+    // Add the 'visible' class to the text element after a short delay
+    setTimeout(function() {
+        // popUpText.forEach(text => {
+        firstName.classList.add('heroTextVisible');
+        // secondText.classList.add('heroTextVisible');
+    }, 50); // Adjust the delay as needed (100ms in this case)
+    // });
+    setTimeout(function() {
+        lastName.classList.add('heroTextVisible');
+    },150);
+    setTimeout(function() {
+        firstHero.classList.add('heroTextVisible');
+    },250);
+    setTimeout(function() {
+        secondHero.classList.add('heroTextVisible');
+    },350);
+
+    
+    setTimeout(function() {
+        pageElements.forEach(elem => {   
+            
+            elem.style.transform = 'translateY(0)';    
+            elem.style.opacity = '1';
+        });
+    },50);
+}
+
+/* --------------------------------------------------------------------------- */
 function getVisibleSlides() {
     if (window.innerWidth >= 1240) {
         return 6; // Show 6 slides on large screens
@@ -16,6 +257,7 @@ function getVisibleSlides() {
     }
 }
 
+/* --------------------------------------------------------------------------- */
 function showSlide(index) {
     const slides = document.querySelectorAll('.slide');
     const totalSlides = slides.length;
@@ -33,65 +275,26 @@ function showSlide(index) {
     document.getElementById('slideshow').style.transform = `translateX(${offset}%)`;
 }
 
+/* --------------------------------------------------------------------------- */
 function moveSlide(step) {
     showSlide(currentSlide + step);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentSlide);
-    setInterval(() => {
-        moveSlide(1);
-    }, 4000); // Change slide every 4 seconds
-
-    window.addEventListener('resize', () => {
-        showSlide(currentSlide);
-    });
-});
-/* -------------------------------- */
-
-/* ---------- Navigation ---------- */
-
-document.getElementById('burgerMenu').addEventListener('click', function() {
-    var navigation = document.querySelector('.navigation');
-    var navLink = document.querySelector('.navContainer > ul');
-    var sideHeader = document.querySelector('.sideHeaderContainer');
-    if (navigation.style.display === 'flex') {
-        navigation.style.display = 'none';
-        navLink.style.display = 'none';
-        // sideHeader.style.display = 'none';
-    } else {
-        navigation.style.display = 'flex';
-        navLink.style.display = 'flex';
-        // sideHeader.style.display = 'block';
-    }
-  });
-  
-
-window.addEventListener('resize', function() {
-    var navigation = document.querySelector('.navigation');
-    var sideHeader = document.querySelector('.sideHeaderContainer');
-    var burgerMenu = document.getElementById('burgerMenu');
+/* --------------------------------------------------------------------------- */
+function switchNav() {
+    const navigation = document.querySelector('.navigation');
+    const burgerMenu = document.getElementById('burgerMenu');
     
     if (window.innerWidth > 768) {
         navigation.style.display = 'flex';
         burgerMenu.style.display = 'none';
-        // sideHeader.style.display = 'none';
     } else {
         navigation.style.display = 'none';
         burgerMenu.style.display = 'flex';
     }
-  });
-/* --------------------------------------- */
+}
 
-/* ---------- Projects Parallax ---------- */
-document.addEventListener('scroll', function() {
-    updateParallax();
-});
-
-window.addEventListener('resize', function() {
-    updateParallax();
-});
-
+/* --------------------------------------------------------------------------- */
 function updateParallax() {
     const scrolled = window.scrollY;
     const descriptions = document.querySelectorAll('[id^="description"]');
@@ -122,125 +325,3 @@ function updateParallax() {
         title.style.top = `${topOffset + (scrolled * titleSpeed2)}px`;
     });
 }
-
-// Initial call to set positions correctly on page load
-updateParallax();
-
-/* --------------------------------------------- */
-  
-/* ----------------- Change Nav Color ----------------- */
-document.addEventListener('DOMContentLoaded', function() {
-    function hoverNav() {
-        const navLinks = document.querySelectorAll('.navLink a');
-
-        navLinks.forEach(link => {
-            if ((isLight))  {
-                link.classList.add('lightHover');
-                link.classList.remove('darkHover');
-            } else if ((!isLight)) {
-                link.classList.add('darkHover');
-                link.classList.remove('lightHover');
-            }
-        });
-    }
-    setInterval(hoverNav, 10);
-});
-
-
-/* ---------- Change Background Color ---------- */
-/* ---------- Nav Color Hover ---------- */
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    var sectionHeaders = document.getElementsByTagName('h1');
-    var sectionNums = document.querySelectorAll('h1 span');
-    var aboutText = document.querySelectorAll('#about p');
-    var contactText = document.querySelectorAll('#contact p, #contact a');
-    var burgerMenu = document.getElementById('burgerMenu');
-
-    function lightScreen() {
-        isLight = true;
-        document.body.style.backgroundColor = 'var(--bgColor)';
-        for (let i = 0; i < sectionHeaders.length; i++) {
-            sectionHeaders[i].style.color = 'black';
-        }
-        sectionNums.forEach(num => {
-            num.style.color = 'var(--tertiaryColor)';
-        });
-        aboutText.forEach(text => {
-            text.style.color = 'black';
-        });
-        contactText.forEach(contactText => {
-            contactText.style.color = 'black';
-        });
-
-        burgerMenu.style.color = 'black';
-    }
-
-    function darkScreen() {
-        isLight = false;
-        document.body.style.backgroundColor = 'var(--secondaryBgColor)';
-        for (let i = 0; i < sectionHeaders.length; i++) {
-            sectionHeaders[i].style.color = 'var(--bgColor)';
-        }
-        sectionNums.forEach(num => {
-            num.style.color = 'var(--primaryShadow)';
-        });
-        aboutText.forEach(aboutText => {
-            aboutText.style.color = 'var(--bgColor)';
-        });
-        contactText.forEach(contactText => {
-            contactText.style.color = 'var(--bgColor';
-        });
-        burgerMenu.style.color = 'var(--bgColor)';
-    }
-
-    function updateNavColor() {
-        const navLinks = document.querySelectorAll('.navLink a');
-        
-        navLinks.forEach(link => {
-    
-            if ((isLight) && (hasChanged < 4)) {
-                link.classList.remove('darkHover');
-                link.classList.add('lightHover');
-                hasChanged++;
-            } else if ((!isLight) && (hasChanged > 0)){
-                link.classList.remove('lightHover');
-                link.classList.add('darkHover');
-                hasChanged--;}
-        });
-    }
-    
-    if (window.innerWidth > 500){
-    
-        if (scrollPosition <= 547) {
-            lightScreen();
-            updateNavColor();
-        } else if (547 < scrollPosition && scrollPosition <= 1426) {
-            lightScreen();
-            updateNavColor();
-        } else if (1426 < scrollPosition && scrollPosition <= 3413) {
-            darkScreen();
-            updateNavColor();
-        } else {
-            lightScreen();
-            updateNavColor();
-        }
-
-    } else{
-        
-        if (scrollPosition <= 309) {
-            lightScreen();
-            updateNavColor();
-        } else if (309 < scrollPosition && scrollPosition <= 1100) {
-            lightScreen();
-            updateNavColor();
-        } else if (1100 < scrollPosition && scrollPosition <= 3087) {
-            darkScreen();
-            updateNavColor();
-        } else {
-            lightScreen();
-            updateNavColor();
-        }
-    }
-});
-/* -------------------------------------------------- */
